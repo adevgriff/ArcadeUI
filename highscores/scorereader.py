4473fbb7649e99510a8cacaf2d9eba5613544044
cfig0219 <100231908+cfig0219@users.noreply.github.com>
2022-11-01 12:32:22 -0400

    input added

--- highscores/scorereader.py
+++ highscores/scorereader.py
@@ -1,36 +1,44 @@
 #imports cvs to allow program to read .cvs score file
 import csv
 
-#opens the high scores file
-with open("./highscores.csv", 'r') as file:
-    #creates lists
-    scores = []
-    ordered = []
+#initializes the high score class
+class HighScore:
 
-    #reads through each line in the file
-    csv_reader = csv.reader(file)
-    for row in csv_reader:
-        #places row number input into scores list
-        nums = row
-        #converts the row input into ints
-        nums = [int(x) for x in nums]
-        scores.append(nums)
+    #takes in the input for the class
+    def __init__(self, listSize):
+        #stores int for number of top scores displayed
+        self.listSize = listSize
 
-    #rearranges order of numbers into ordered list
-    ordered = sorted(scores)
-    ordered.reverse()
-    #print(ordered)
+    #opens the high scores file
+    with open("./highscores.csv", 'r') as file:
+        #creates lists
+        scores = []
+        ordered = []
 
-    #creates window to display top 5 high scores
-    import tkinter as tk
-    window = tk.Tk()
-    window.title("High Scores")
+        #reads through each line in the file
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            #places row number input into scores list
+            nums = row
+            #converts the row input into ints
+            nums = [int(x) for x in nums]
+            scores.append(nums)
 
-    for element in ordered:
-        for item in element:
-            items = item
-            #still needs to only read top 5, reads all for now
-            greeting = tk.Label(text=(item))
-            greeting.pack()
+        #rearranges order of numbers into ordered list
+        ordered = sorted(scores)
+        ordered.reverse()
+        #print(ordered)
 
-    window.mainloop()
+        #creates window to display top 5 high scores
+        import tkinter as tk
+        window = tk.Tk()
+        window.title("High Scores")
+
+        for element in ordered:
+            for item in element:
+                items = item
+                #reads the scores onto a window
+                greeting = tk.Label(text=(item))
+                greeting.pack()
+
+        window.mainloop()
